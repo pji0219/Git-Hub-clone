2542f25124a92fac872ef23c8882d67c1e68adf0
park jong ik <pji219@naver.com>
2020-12-10 16:28:02 +0900

    미디어 쿼리, 토글 버튼 작업

---
+++ main.js
@@ -0,0 +1,19 @@
+// 즉시 실행 함수, 전체 영역을 더럽히지 못하게 부분화(모듈화) 하는 것
+(function(window, document) {
+    'use strict'; // 엄격한 자바스크립트 문법에 의해서 작성 하겠다는 선언
+    
+    // HTML 요소를 담을 상수명 앞에 달러 기호를  써줘도 된다.
+    const $toggles = document.querySelectorAll('.toggle'); // NodeList라는 유사 배열 형태
+    const $toggleBtn = document.getElementById('toggle-btn');
+
+    $toggleBtn.addEventListener('click', function() {
+        toggleElements();
+    });
+
+    function toggleElements() {
+        [].forEach.call($toggles, function(toggle) {
+            toggle.classList.toggle('on'); // 클래스리스트는 클래스를 제어하는 속성인데 on이라는 클래스 선택자를 토클하겠다.
+        });
+
+    }
+})(window, document)
